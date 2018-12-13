6a13591bd5896aacafc336e9811b8ed3e11bd7bb
connor <connor.jaksik@gmail.com>
2018-12-13 14:34:46 -0700

    control check

--- letter.js
+++ letter.js
@@ -1,25 +1,30 @@
 function Letter(value) {
-    this.letter = value;
+//     A string value to store the underlying character for the letter
+    this.character = value;
+//     A boolean value that stores whether that letter has been guessed yet
     this.guessed = false;
-    this.toString = function () {
-
-        if (this.letter === " ") {
-            this.guessed = true;
-            return " ";
+//     A function that returns the underlying character if the letter has been guessed, or a placeholder (like an underscore) if the letter has not been guessed
+    this.check = function () {
+        if (!this.guessed) {
+            return "_"
         } else {
-            if (this.guessed === false) {
-                return "_";
-            } else {
-                return this.letter;
-            }
+            return this.character
         }
-    };
-    
-    this.guess = function (guess) {
-        if (guess === this.letter) {
-            this.guessed = true;
+    }
+//     A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
+    this.guess = function (char) {
+        if (this.character === char) {
+            this.guessed = true
         }
     }
 }
 
+
+var words = [];
+var
+var exampleLetter = new Letter("s")
+var exampleTwo = new Letter("B")
+console.log(exampleLetter.check())
+console.log(exampleTwo.character)
+console.log(exampleLetter.guess("i"))
 module.exports = Letter;
